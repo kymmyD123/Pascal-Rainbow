4ed0c916078e622f0c1a6569ea5420a9665e0cc5
Valera <kymmyd123@gmail.com>
2016-10-08 22:10:01 +0300

    pascal-rainbow

---
+++ Program2.pas
@@ -0,0 +1,21 @@
+Program F3;
+Uses GraphABC;
+Begin
+  SetBrushColor(clRed);
+  Ellipse(280,280,0,0);
+  SetBrushColor(clOrange);
+  Ellipse(260,260,20,20);
+  SetBrushColor(clYellow);
+  Ellipse(240,240,40,40);
+  SetBrushColor(clGreen);
+  Ellipse(220,220,60,60);
+  SetBrushColor(clSkyBlue);
+  Ellipse(200,200,80,80);
+  SetBrushColor(clBlue);
+  Ellipse(180,180,100,100);
+  SetBrushColor(clPurple);
+  Ellipse(160,160,120,120);
+  SetBrushColor(clWhite);
+  SetPenColor(clWhite);
+  Rectangle(0,140,280,280);
+End.
